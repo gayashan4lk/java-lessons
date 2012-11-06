ecbff664a823589715369465cd4cb78606e9decc
Gayashan Wagachchi <gayashan4lk@gmail.com>
2012-11-06 18:16:01 -0400

    chapter1

---
+++ netbeans-projects/chapter1/src/series.java
@@ -0,0 +1,14 @@
+class series{
+	public static void main(String args[]){
+	
+	System.out.println("Index \t Value");
+	
+	int numbers[] = {5,3,7,2,9,10,11,4};
+	
+	for(int counter = 0; counter<numbers.length ;counter++ )
+	{
+		//System.out.println(numbers[counter]);
+		System.out.println(counter + "\t  "+ numbers[counter]);
+	}
+	}
+}
